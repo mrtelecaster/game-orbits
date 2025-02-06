ead73389517e5d8ad6df69a7bc2f4f508dbcf90e
Fernando A. Fraticelli <53105809+mrtelecaster@users.noreply.github.com>
2025-02-06 15:51:25 -0500

    feat: add rotation control to solar system example

--- examples/solar_system.rs
+++ examples/solar_system.rs
@@ -3,6 +3,10 @@ use bevy::prelude::*;
 use game_orbits::Database;
 
 
+const CAM_ROTATE_LEFT: KeyCode = KeyCode::ArrowLeft;
+const CAM_ROTATE_RIGHT: KeyCode = KeyCode::ArrowRight;
+const CAM_ROTATE_SPEED: f32 = 1.0; // rad/s
+
 const ORBIT_SEGMENTS: usize = 100;
 const ORBIT_COLOR: Color = Color::srgb(0.5, 1.0, 0.0);
 const PERIAPSIS_COLOR: Color = Color::srgb(1.0, 0.5, 0.0);
@@ -11,12 +15,51 @@ const PLANET_COLOR: Color = Color::srgb(1.0, 1.0, 1.0);
 const SUN_COLOR: Color = Color::srgb(1.0, 1.0, 0.0);
 const APSIS_SIZE: f32 = 0.2;
 
+#[derive(Component)]
+struct CameraParent {
+	pub rotation: f32,
+}
+impl Default for CameraParent {
+	fn default() -> Self {
+		Self{ rotation: 0.0 }
+	}
+}
+
 
 fn setup_camera(mut commands: Commands) {
-	commands.spawn((
+	// camera
+	let camera = commands.spawn((
 		Camera3d::default(),
-		Transform::from_xyz(50.0, 50.0, -100.0).looking_at(Vec3::ZERO, Vec3::Y),
-	));
+		Transform::from_xyz(50.0, 35.0, -100.0).looking_at(Vec3::ZERO, Vec3::Y),
+	)).id();
+	// camera parent
+	commands.spawn((
+		Transform::default(),
+		CameraParent::default(),
+	)).add_child(camera);
+}
+
+fn process_input(
+	keyboard: Res<ButtonInput<KeyCode>>, time: Res<Time>,
+	mut camera_parents: Query<&mut CameraParent>,
+){
+	let delta = time.delta_secs();
+	let mut camera_parent = camera_parents.single_mut();
+	if keyboard.pressed(CAM_ROTATE_RIGHT) {
+		camera_parent.rotation += CAM_ROTATE_SPEED * delta;
+	}
+	if keyboard.pressed(CAM_ROTATE_LEFT) {
+		camera_parent.rotation -= CAM_ROTATE_SPEED * delta;
+	}
+}
+
+fn update_camera_position(
+	mut camera_parents: Query<(&mut Transform, &CameraParent), Without<Camera3d>>,
+	mut cameras: Query<&mut Transform, (With<Camera3d>, Without<CameraParent>)>,
+){
+	let (mut camera_parent_transform, camera_parent) = camera_parents.single_mut();
+	let mut _camera = cameras.single_mut();
+	camera_parent_transform.rotation = Quat::from_axis_angle(Vec3::Y, camera_parent.rotation);
 }
 
 fn draw_orbits(mut gizmos: Gizmos, db: Res<Database<usize, f32>>) {
@@ -66,6 +109,9 @@ fn main() {
 		.add_plugins(DefaultPlugins)
 		.insert_resource(Database::<usize, f32>::solar_system())
 		.add_systems(Startup, setup_camera)
-		.add_systems(Update, (draw_orbits, draw_planets, draw_axis))
+		.add_systems(Update, (
+			draw_orbits, draw_planets, draw_axis,
+			process_input, update_camera_position.after(process_input),
+		))
 		.run();
 }
